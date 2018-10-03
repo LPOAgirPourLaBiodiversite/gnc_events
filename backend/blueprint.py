5014a5c369aa98f6c34c0efba8ca46059bdcf791
fred.perso <fcloitre@ardesca.fr>
2018-10-04 00:18:43 +0200

    update routes and models

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -1,14 +1,15 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
+from flask_jwt_extended import (jwt_optional)
 from geoalchemy2.shape import from_shape
-from marshmallow import ValidationError
+from geojson import FeatureCollection
 from shapely.geometry import Point
+from shapely.geometry import asShape
 
+from gncitizen.utils.utilsjwt import get_id_role_if_exists
 from gncitizen.utils.utilssqlalchemy import get_geojson_feature, json_resp
 from server import db
 from .models import EventModel
-from .schemas import event_schema
-from datetime import datetime
-from datetime import date
+
 blueprint = Blueprint('events', __name__)
 
 
@@ -17,24 +18,47 @@ blueprint = Blueprint('events', __name__)
 # def hello():
 #     return "Hello World!"
 
-@blueprint.route('/', methods=['GET'])
+@blueprint.route('/<int:pk>', methods=['GET'])
 @json_resp
-def get_events():
-    """
-    List all events
+def get_event(pk):
+    """Get an event by id
     ---
     tags:
       - Evènements (Module externe)
+    parameters:
+      - name: pk
+        in: path
+        type: integer
+        required: true
+        example: 1
+    definitions:
+      properties:
+        type: dict
+        description: event properties
+      geometry:
+        type: geojson
+        description: GeoJson geometry
     responses:
       200:
-        description: A list of all sights
+        description: A sight detail
     """
-    events = EventModel.query.all()
-    return events
+    try:
+        result = EventModel.query.get(pk)
+        result_dict = result.as_dict(True)
+        features = []
+        feature = get_geojson_feature(result.geom)
+        for k in result_dict:
+            if k not in ('id_creator', 'geom'):
+                feature['properties'][k] = result_dict[k]
+            features.append(feature)
+        return {'features': features}, 200
+    except Exception as e:
+        return {'error_message': str(e)}, 400
 
 
 @blueprint.route('/', methods=['POST'])
-# @jwt_optional
+@json_resp
+@jwt_optional
 def post_event():
     """Gestion des évènements
     If method is POST, add a sight to database else, return all sights
@@ -65,10 +89,10 @@ def post_event():
                 - picture
               properties:
                 date:
-                  type: date
+                  type: string
                   description: Date
-                  format: date
-                  example: 2018-09-15
+                  required: true
+                  example: "2018-09-20"
                 time:
                   type: string
                   default:  none
@@ -89,86 +113,88 @@ def post_event():
                 contact_phone:
                   type: string
                   example: +33999999999
-                geom:
+                geometry:
                   type: string
                   example: {"type":"Point", "coordinates":[45,5]}
         responses:
           200:
             description: Event created
         """
-    json_data = request.get_json()
-    medias = request.files
-    if not json_data:
-        return jsonify({'message': 'No input data provided'}), 400
-    # Validate and deserialize input
-    # info: manque la date
     try:
-        data, errors = event_schema.load(json_data)
-    except ValidationError as err:
-        return jsonify(err.messages), 422
-    try:
-        # Trouver le moyen d'intégrer le datetime
-        date = date(datetime.now())
-    except:
-        return jsonify('Date incomplète'), 422
-    try:
-        organizer = data['organizer']
-    except:
-        return jsonify('organizer incomplète'), 422
-    try:
-        geom = from_shape(Point(data['geom']['coordinates']), srid=4326)
-    except:
-        return jsonify('geom incomplète'), 422
-    try:
-        thematic = data['thematic']
-    except:
-        return jsonify('thematic incomplète'), 422
-    try:
-        description = data['description']
-    except:
-        return jsonify('description incomplète'), 422
-    try:
-        time = data['time']
-    except:
-        return jsonify('description incomplète'), 422
-    url = data['url']
-    contact_email = data['contact_email']
-    contact_phone = data['contact_phone']
-    picture = data['picture']
-    # try:
-    #     query = LiMunicipalities.query.join(LAreas, LAreas.ip_area == LiMunicipalities.id_area).add_columns(LiMunicipalities.nom_com).first()
-    #     print(query)
-    #     # municipality = db.session.query(query).filter(func.ST_Intersects(query.geom, geom))
-    # except:
-    #     return jsonify('impossible de trouver la commune'), 422
+        request_datas = dict(request.get_json())
 
-    # Si l'utilisateur est connecté, attribut ajoute l'id_role de l'utilisateur.
-    # Sinon, complète le champ obs_txt.
-    # Si obs_txt est vice, indique 'Anonyme'
+        if request.files:
+            file = request.files['file']
+            file.save()
+        else:
+            file = None
 
-    # Create new sight
-    event = EventModel(
-        date=date,
-        organizer=organizer,
-        geom=geom,
-        thematic=thematic,
-        description=description,
-        time=time,
-        url=url,
-        contact_email=contact_email,
-        contact_phone=contact_phone,
-        picture=picture
-    )
+        datas2db = {}
+        for field in request_datas:
+            if hasattr(EventModel, field):
+                datas2db[field] = request_datas[field]
 
-    db.session.add(event)
-    db.session.commit()
-    # Réponse en retour
-    result = EventModel.query.get(event.id_event)
-    features = []
-    feature = get_geojson_feature(result.geom)
-    feature['properties'] = result.as_dict(True)
-    features.append(feature)
-    return jsonify({
-        'message': 'New sight created.',
-        'features': features,
-    }), 200
+        try:
+            newevent = EventModel(**datas2db)
+        except Exception as e:
+            return {'error_message': str(e)}, 400
+
+        try:
+            shape = asShape(request_datas['geometry'])
+            newevent.geom = from_shape(Point(shape), srid=4326)
+        except Exception as e:
+            return {'error_message': str(e)}, 400
+
+        newevent.id_creator = get_id_role_if_exists()
+
+        db.session.add(newevent)
+        db.session.commit()
+        # Réponse en retour
+        result = EventModel.query.get(newevent.id_event)
+        result_dict = result.as_dict(True)
+        features = []
+        feature = get_geojson_feature(result.geom)
+        for k in result_dict:
+            if k not in ('id_creator', 'geom'):
+                feature['properties'][k] = result_dict[k]
+        features.append(feature)
+        return {
+                   'message': 'New event created.',
+                   'features': features,
+               }, 200
+    except Exception as e:
+        return {'error_message': str(e)}, 400
+
+
+@blueprint.route('/', methods=['GET'])
+@json_resp
+def get_events():
+    """Get all events
+    ---
+    tags:
+      - Evènements (Module externe)
+    definitions:
+      FeatureCollection:
+        properties:
+          type: dict
+          description: event properties
+        geometry:
+          type: geojson
+          description: GeoJson geometry
+    responses:
+      200:
+        description: A sight detail
+    """
+    try:
+        events = EventModel.query.all()
+        features = []
+        for event in events:
+            feature = get_geojson_feature(event.geom)
+            event_dict = event.as_dict(True)
+            for k in event_dict:
+                if k not in ('id_creator', 'geom'):
+                    feature['properties'][k] = event_dict[k]
+                features.append(feature)
+        return FeatureCollection(features)
+    except Exception as e:
+        return {'error_message': str(e)}, 400
--- backend/models.py
+++ backend/models.py
@@ -12,15 +12,16 @@ class EventModel(db.Model):
     __tablename__ = 'events'
     __table_args__ = {'schema': 'gncitizen'}
     id_event = db.Column(db.Integer, primary_key=True, unique=True)
-    organizer = db.Column(db.Integer, db.ForeignKey('taxonomie.bib_noms.cd_nom'))
+    organizer = db.Column(db.String(200))
     thematic = db.Column(db.String(200))
     description = db.Column(db.String(300))
     date = db.Column(db.DATE, nullable=False)
-    time = db.Column(db.Integer, db.ForeignKey('gncitizen.users.id_user'))
+    time = db.Column(db.TIME)
     url = db.Column(db.String(150))
     contact_email = db.Column(db.String(150))
     contact_phone = db.Column(db.String(150))
     picture = db.Column(db.Text)
     geom = db.Column(Geometry('POINT', 4326))
     municipality = db.Column(db.String(5), db.ForeignKey('ref_geo.li_municipalities.id_municipality'))
+    id_creator = db.Column(db.Integer, db.ForeignKey('gncitizen.users.id_user'))
     timestamp_create = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
